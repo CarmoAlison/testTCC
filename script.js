6822a3036ee419ba0a6167871f3fc0f36a4cdc7d
CarmoAlison <157990201+CarmoAlison@users.noreply.github.com>
2024-04-30 21:39:25 -0300

    teste TCC

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function MenuLateral(){
+    if(document.menuLateral.style.display == 'none'){
+        document.menuLateral.style.display = 'block';
+    }else{
+        document.menuLateral.style.display = 'none';
+    }
+
+}
